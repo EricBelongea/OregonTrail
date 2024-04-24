594230721aea06a06c40449d542109d157667695
Eric Belongea <belongeae@infinity-software.com>
2024-04-24 08:28:06 -0600

    Finishes the initial game.

--- Models/Utilities.cs
+++ Models/Utilities.cs
@@ -16,9 +16,95 @@ namespace OregonTrail.Models
         {
             _context = context;
         }
-        public async Task<ICollection<Caravan>> LoadAllCaravans()
+        public async Task<List<Caravan>> LoadAllCaravans()
         {
             return await _context.Caravan.ToListAsync();
+        }
+
+        public Caravan GetCaravanWithWagon(int caravanId)
+        {
+            return _context.Caravan.Include(c => c.Wagons).FirstOrDefault(c => c.CaravanId == caravanId);
+        }
+
+        public Caravan GetCaravanWithWagonAndPassengers(int caravanId)
+        {
+            return _context.Caravan.Include(c => c.Wagons).ThenInclude(w => w.Passangers).FirstOrDefault(c => c.CaravanId == caravanId);
+        }
+
+        public void CountOfWagons(int caravanId)
+        {
+            Caravan caravan = GetCaravanWithWagon(caravanId);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"In '{caravan.Name}' there are {caravan.Wagons.Count} wagons");
+            Console.WriteLine("What else would you like to do?");
+        }
+
+        public void PassengersPerWagon(int caravanId)
+        {
+            Caravan caravan = GetCaravanWithWagonAndPassengers(caravanId);
+            foreach (Wagon wagon in caravan.Wagons)
+            {
+                Console.WriteLine($"In wagon '{wagon.Name}' there are {wagon.Passangers.Count()} passengers");
+            }
+        }
+
+        public void UniqueDestinations(int caravanId)
+        {
+            List<string> uniqueDestinations = _context.Caravan
+                .Where(c => c.CaravanId == caravanId)
+                .SelectMany(c => c.Wagons.SelectMany(w => w.Passangers.Select(p => p.Destination)))
+                .Distinct()
+                .ToList();
+            Console.WriteLine($"There are {uniqueDestinations.Count} Unique Destinations");
+            for (int i = 0; i < uniqueDestinations.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {uniqueDestinations[i]}");
+            }
+        }
+
+        public void AverageAgeOfAll(int caravanId)
+        {
+            int age = (int)_context.Caravan.Where(c => c.CaravanId == caravanId)
+                .SelectMany(c => c.Wagons.SelectMany(w => w.Passangers))
+                .Average(p => p.Age);
+            Console.WriteLine($"The average age for this caravan is {age}");
+        }
+
+        public void AverageAgeEachWagon(int caravanId)
+        {
+            var averageAgeByWagon = _context.Caravan
+                 .Where(c => c.CaravanId == caravanId)
+                 .SelectMany(c => c.Wagons)
+                 .Select(w => new
+                 {
+                     WagonName = w.Name,
+                     AverageAge = (int?)w.Passangers.Average(p => p.Age)
+                 })
+                 .ToList();
+            for (int i = 0; i < averageAgeByWagon.Count; i++)
+            {
+                Console.WriteLine($"In wagon '{averageAgeByWagon[i].WagonName}' \nThe Average age is {averageAgeByWagon[i].AverageAge}");
+            }
+        }
+
+        public void PassengerAndWagonNames(int caravanId)
+        {
+            var roster = _context.Caravan
+                .Where(c => c.CaravanId == caravanId)
+                .SelectMany(c => c.Wagons
+                .Select(w => new
+                {
+                    WagonName = w.Name,
+                    PassengerNames = w.Passangers.Select(p => p.Name).ToList()
+                })).ToList();
+            for (int i = 0; i < roster.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. In wagon '{roster[i].WagonName}'");
+                foreach (var p in roster[i].PassengerNames)
+                {
+                    Console.WriteLine($"- {p}");
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,8 +10,59 @@ string work = Console.ReadLine();
 Utilities utilities = new Utilities(new CaravanContext());
 if (work == "y")
 {
-    ICollection<Caravan> caravans = await utilities.LoadAllCaravans();
-    Console.WriteLine($"There are {caravans.Count} Caravans awaiting you");
+    List<Caravan> caravans = await utilities.LoadAllCaravans();
+    Console.WriteLine($"There are {caravans.Count} Caravan(s) awaiting you");
+    Console.WriteLine("Which Caravan would you like to inspect?");
+    for (int i = 0; i < caravans.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. '{caravans[i].Name}'");
+    }
+    int index = int.Parse(Console.ReadLine());
+    string userSelection;
+    Console.WriteLine($"In Caravan '{caravans[index - 1].Name}', what would you like to do?");
+    do
+    {
+        Console.WriteLine("-----------------------------------------");
+        Console.WriteLine("1. Count how many wagons?");
+        Console.WriteLine("2. Count of passangers in each wagon?");
+        Console.WriteLine("3. List of Destinations");
+        Console.WriteLine("4. Average Age of all passengers");
+        Console.WriteLine("5. Average age of each wagon");
+        Console.WriteLine("6. List of all name associated with their wagon");
+        Console.WriteLine("9. Quit Application");
+        Console.WriteLine("-----------------------------------------");
+
+        userSelection = Console.ReadLine();
+        int caravanId = caravans[index - 1].CaravanId;
+
+        switch (userSelection)
+        {
+            case "1":
+                utilities.CountOfWagons(caravanId);
+                break;
+            case "2":
+                utilities.PassengersPerWagon(caravanId);
+                break;
+            case "3":
+                utilities.UniqueDestinations(caravanId);
+                break;
+            case "4":
+                utilities.AverageAgeOfAll(caravanId);
+                break;
+            case "5":
+                utilities.AverageAgeEachWagon(caravanId);
+                break;
+            case "6":
+                utilities.PassengerAndWagonNames(caravanId);
+                break;
+            case "9":
+                break;
+            default:
+                Console.WriteLine("Invalid Selection. Please Try Again");
+                break;
+        }
+    }
+    while (userSelection != "9");
 }
 
 Console.WriteLine("See ya later Partner");
